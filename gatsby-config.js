9560331ec3700892556eb6b5ba05049fad48617e
mattjford1 <57039477+mattjford1@users.noreply.github.com>
2020-10-05 11:25:52 +0100

    Revert "Amended dotev config"
    This reverts commit 35b1524645eb72fea3b2890895471c7904164e8e.

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,9 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+require('dotenv').config({
+  path: `.env`
+});
 
 module.exports = {
   /* Your site config here */

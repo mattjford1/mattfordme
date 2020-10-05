7f9b2f130c640cf088071d370f0eb22a23ab9649
mattjford1 <57039477+mattjford1@users.noreply.github.com>
2020-10-05 11:40:09 +0100

    Revert "Further reverts"
    This reverts commit 1f2f2a85c37b3fd2661c94475aa6718c743f4023.

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,6 +70,30 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `sg5rvwjajl1u`,
+        accessToken: process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN,
+        downloadLocal: true,
+        forceFullSync: true,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `sg5rvwjajl1u`,
+        accessToken: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
+        host: `preview.contentful.com`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-mailchimp',
+        options: {
+            endpoint: process.env.MAILCHIMP_ENDPOINT,
+            timeout: 3500,
+          },
+    },
+    {
       resolve: `gatsby-plugin-metomic`,
       options: {
         clientId: process.env.METOMIC_CLIENT_ID,
